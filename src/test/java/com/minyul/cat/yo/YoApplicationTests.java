7b7b6cc5f444cd02d3c4ef56c85fb42f0f5d852b
minyul <ggomjae@gmail.com>
2022-03-20 21:17:25 +0900

    init

---
+++ src/test/java/com/minyul/cat/yo/YoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minyul.cat.yo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
